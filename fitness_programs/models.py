e896b5aea51c12431f1bfea38a1ff8f1d7aed5f4
Ubuntu <ubuntu@ip-172-31-92-251.ec2.internal>
2019-08-16 08:16:46 +0000

    make all tests.py to pass so travis test to pass as well

--- fitness_programs/models.py
+++ fitness_programs/models.py
@@ -108,4 +108,5 @@ class fitness_programs(models.Model):
    
 
     
-    
+   def __str__(self):
+        return '%s %s' % (self.name, self.price) 
--- fitness_programs/tests.py
+++ fitness_programs/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
-from .models import fitness_program
+from .models import fitness_programs
 
 # Create your tests here.
+
+#test again input in fitness_programs model
 
 class Fitness_ProgramsTests(TestCase):
     """ testing again fitness_programs model"""
     def test_str(self):
-        test_name = fitness_program(day1='alfa', day2='beta')
-        self.assertEquals(str(test_name), 'alfa beta')
+        test_name = fitness_programs(name='alfa', price='27')
+        self.assertEquals(str(test_name), 'alfa 27')
